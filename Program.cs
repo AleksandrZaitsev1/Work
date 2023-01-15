33fa4190ea286fddb8cba197d7d9dc9c53110805
Aleksandr <zaitsey@yandex.ru>
2023-01-15 15:49:47 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,63 +1,23 @@
-﻿//int a;
-
-//string? str = Console.ReadLine();
-
-//a = Convert.ToInt32(str);
-
-//Console.WriteLine(a * a);
-
+//System.Console.Write("-->");
 //int a = Convert.ToInt32(Console.ReadLine());
-//int b  = Convert.ToInt32(Console.ReadLine());
-//if (a == b*b)
+
+//int i = -1 * a;
+
+//while (i<= a)
 //{
-  //   Console.WriteLine($"{a} квадрат {b}");
-    // }
-    // else
-    // {
-      //  System.Console.WriteLine("не квадрат!");
-     
-     //}
+    //System.Console.WriteLine(i);
+  //  i++;
+//};
+System.Console.Write("-->");
+int a = Convert.ToInt32(Console.ReadLine());
 
-     //System.Console.WriteLine("Введите число от 1 до 7");
-     //int a = Convert.ToInt32(Console.ReadLine());
-     
-    // switch (a){
+if ( a < 100 || a > 999){
+    System.Console.WriteLine("Ошибка");
 
-        //case 1:
-        //System.Console.WriteLine("Пнедельник");
-        //break;
+}
 
-        //case 2:
-         //System.Console.WriteLine("Вторник");
-         //break;
-
-         //case 3:
-         //System.Console.WriteLine("Среда");
-         //break;
-
-         //case 4:
-          //System.Console.WriteLine("Четверг");
-        // break;
-
-         //case 5:
-         // System.Console.WriteLine("Пятница");
-         //break;
-
-         //case 6:
-         // System.Console.WriteLine("Суббота");
-         //break;
-
-         //case 7:
-         // System.Console.WriteLine("Воскресенье");
-         //break;
-
-         //default:
-
-        //  System.Console.WriteLine("ВВедеино не верное значение");
-      //   break;
-
-     
-
-
-
-    // }
+else
+{
+    int b = a % 10;
+    System.Console.WriteLine(b);
+}
